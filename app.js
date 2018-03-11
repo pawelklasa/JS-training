a137d2812dbe9dddc5904ba5328b5a09ed4da45c
Pawel <pawel.klasa@gmail.com>
2018-03-11 18:43:08 +0000

    else

---
+++ app.js
@@ -0,0 +1,15 @@
+// if(something){
+//   do something
+// } else {
+//   do something else
+// }
+
+const id = 100;
+
+// equal to
+if(id == 101){
+  console.log('Correct!');
+} else {
+  console.log('Wrong!');
+}
+
