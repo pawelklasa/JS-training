9234734aee88ad178d95c1dc989d6f14d044cdf4
Pawel <pawel.klasa@gmail.com>
2018-03-11 17:06:19 +0000

    template

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const name = 'John';
 const age = 30;
-const jog = 'Web Developer';
+const job = 'Web Developer';
 const city = 'London';
+
+let html;
+
+// Without template strings (es5)
+html = '<ul><li>Hame: ' + name + ' </li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
+
+// to put everything neatly on seperate lines you need to conatenate
+html = '<ul>' + 
+        '<li>Hame: ' + name + ' </li>' +
+        '<li>Age: ' + age + ' </li>' +
+        '<li>Job: ' + job + ' </li>' +
+        '<li>City: ' + city + ' </li>' +
+        '</ul>';
+
+document.body.innerHTML = html;
+
+
+
 
