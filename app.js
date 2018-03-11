723166dd8fe0cb635224c3951f154ccf1e571eae
Pawel <pawel.klasa@gmail.com>
2018-03-11 17:28:12 +0000

    random array

--- app.js
+++ app.js
@@ -2,3 +2,4 @@
 const numbers = [43, 56, 33, 23, 44, 36, 5];
 const numbers2 = new Array(22, 45, 33, 76, 54); // create an array using constructor
 const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [22, 'Hello', true, undefined, null, {a:1, b:2}, new Date()];
