36127edf226ced204ec9384f96e3831413d3844a
Pawel <pawel.klasa@gmail.com>
2018-03-11 16:25:05 +0000

    append

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const firstName = 'Pawel';
 const lastName = 'Klasa';
+const age = 36;
 
 let val;
 
+// Concatenation
+
 val = firstName + ' ' + lastName;
+
+// Append
+val = 'Brad ';
+val += 'Traversy';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+
+
 
 console.log(val);
