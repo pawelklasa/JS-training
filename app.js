9247f8f583385fc7f8eefa8d49a933a66f623215
Pav <pawel.klasa@gmail.com>
2018-03-12 05:17:23 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const pawel = {
-  name: 'Pawel',
-  surname: ['Klasa', 'Klassa'],
-  age: 35,
-  city: 'London'
-}
 
-let val;
-
-val = pawel.surname[1];
-
-document.body.innerHTML = val;
-
-console.log(val);
