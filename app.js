89af4dbb4972e0478eff65f6cbc4c1aed3548612
Pawel <pawel.klasa@gmail.com>
2018-03-12 02:28:16 +0000

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const pawel = {
+  name: 'Pawel',
+  surname: ['Klasa', 'Klassa'],
+  age: 35,
+  city: 'London'
+}
+
+let val;
+
+val = pawel.surname[1];
+
+document.body.innerHTML = val;
+
+console.log(val);
+
