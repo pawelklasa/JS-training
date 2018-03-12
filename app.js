b2cb5c6a66b5ea37cf7a3e98551ffaa857cbea1e
Pawel <pawel.klasa@gmail.com>
2018-03-12 05:13:55 +0000

    switch end

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const pawel = {
-  name: 'Pawel',
-  surname: ['Klasa', 'Klassa'],
-  age: 35,
-  city: 'London'
+const color = 'blue';
+
+switch(color){
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('No color detected');
+    break;
 }
 
-let val;
+let day;
 
-val = pawel.surname[1];
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+    break;
+}
 
-document.body.innerHTML = val;
-
-console.log(val);
-
+console.log(`Today is ${day}`);
